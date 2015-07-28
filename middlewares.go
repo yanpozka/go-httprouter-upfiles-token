c9b4fbf8040e00d869722e91f0cfb3dfe81f2543
Yan <ypozoka@gmail.com>
2015-07-28 18:12:47 -0400

    Missing middlewares

---
+++ middlewares.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+//
+type CommonMiddleware func(http.ResponseWriter, *http.Request) error
+
+//
+// TODO: remove it. Use https://github.com/julienschmidt/httprouter#basic-authentication
+//
+func ValidateBasicAuth(u, p string) string {
+	if u == "yandry" {
+		// password is "hello"
+		return "$1$dlPL2MqE$oQmn16q49SqdmhenQuNgs1"
+	}
+	return ""
+}
+
+//
+func Logger(inner http.Handler, name string) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		ruri := r.RequestURI
+
+		//
+		inner.ServeHTTP(w, r)
+
+		log.Printf("%s: %s (%s). Time consumed: %s", r.Method, ruri, name, time.Since(start))
+	})
+}
+
+// Token-accesss protected
+func TokenAccessVerification(inner http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if hd, found := r.Header["Authorization"]; !found {
+			http.Error(w, "Missing 'Authorization' header", http.StatusUnauthorized)
+			return
+		} else if len(hd) > 0 && !strings.Contains(hd[0], "Token ") {
+			http.Error(w, "Invalid format for 'Authorization' header", http.StatusUnauthorized)
+			return
+		}
+
+		//
+		inner.ServeHTTP(w, r)
+	})
+}
+
+//
+func CommonHeaders(resw http.ResponseWriter, req *http.Request) error {
+	header := resw.Header()
+	header.Set("Server", "Yandry")
+	header.Set("X-Powered-By", "Yandry-Server 0.1")
+
+	// default application/json for all responses
+	header.Set("Content-Type", "application/json")
+
+	return nil
+}
